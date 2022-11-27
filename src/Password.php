23e9fb9a5730f6a1ef2ca02b0cba98fac88ee98d
gemapatricia <gp.herrera@usp.ceu.es>
2022-11-27 18:10:11 +0100

    Test 11 ok

--- src/Password.php
+++ src/Password.php
@@ -17,7 +17,8 @@ Class Password{
         if($this->comprobarLongitud($cadena)) $condiciones += 1;
         if($this->comprobarDosNumeros($cadena)) $condiciones += 1;
         if($this->comprobarMayuscula($cadena)) $condiciones += 1;
-        if ($condiciones == 3) $validez=true;
+        if($this->comprobarCaracterEspecial($cadena)) $condiciones += 1;
+        if ($condiciones == 4) $validez=true;
         return $validez;
     }
 
@@ -57,6 +58,19 @@ Class Password{
         if (!$validez){
             $this->error[$this->posicion] = "No hay mayúsculas";
             $this->posicion += 1;
+        }
+        return $validez;
+    }
+
+    public function comprobarCaracterEspecial(string $cadena):bool{
+        $validez = false;
+        $pattern = '/[\'^£$%&*()}{@#~?><>,|=_+¬-]/';
+        
+        foreach (str_split($cadena) as $value){
+            if (preg_match($pattern,$value)){
+                $validez=true;
+                break;
+            }
         }
         return $validez;
     }
